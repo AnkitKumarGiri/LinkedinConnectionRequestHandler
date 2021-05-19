b823f182151e493e8eea056801cfcbda0a4ae656
Ankit Kumar Giri <peace.247ankit@gmail.com>
2021-05-19 22:29:52 +0530

    added pop_up trigger

--- dist/content.js
+++ dist/content.js
@@ -1,29 +1,31 @@
 
-const automateRequestHandling = async connection_count_thresh => {
-    
-    console.log("Hello World");
-}
+chrome.storage.sync.get("conn_cnt_thresh", ({ conn_cnt_thresh }) => {
+    console.log(conn_cnt_thresh);
+
     // select all requests
-    // var all_requests = document.getElementsByClassName("invitation-card artdeco-list__item");   
-
-    // for (const request of all_requests){
+    var all_requests = document.getElementsByClassName("invitation-card artdeco-list__item");
+    
+    for (const request of all_requests){
         
-    //     // find number of mutual connections
-    //     var mutual_conn_count = request.getElementsByClassName("member-insights__count")[0].innerText;
-    //     mutual_conn_count = mutual_conn_count.replace(/\D/g, '');
+        // find number of mutual connections
+        var mutual_conn_text = request.getElementsByClassName("member-insights__count")[0];
+        var mutual_conn_count = "0";
 
-    //     console.log(mutual_conn_count);
+        if (mutual_conn_text != null){
+            mutual_conn_count = mutual_conn_text.innerText;
+        }
 
-    //     // hit accept or ignore based on condition
-    //     var action_buttons = request.getElementsByClassName("invitation-card__action-btn");
-    //     if (mutual_conn_count >= connection_count_thresh){
-    //         action_buttons[1].click();
-    //     }
-    //     else{
-    //         action_buttons[0].click();
-    //     }
-    //     break;
-    // }
-// }
+        mutual_conn_count = mutual_conn_count.replace(/\D/g, '');
 
-export {automateRequestHandling};
+        console.log(mutual_conn_count);
+
+        // hit accept or ignore based on condition
+        var action_buttons = request.getElementsByClassName("invitation-card__action-btn");
+        if (mutual_conn_count >= conn_cnt_thresh){
+            action_buttons[1].click();
+        }
+        else{
+            action_buttons[0].click();
+        }
+    }
+});
--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,16 @@
-import {automateRequestHandling} from './content';
-
 const form = document.querySelector(".form-data");
-const connection_count_thresh = document.querySelector("cnt-thresh");
+const connection_count_thresh = document.querySelector(".cnt-thresh");
 
-const handleSubmit = async e => {
-    e.preventDefault();
-    automateRequestHandling(connection_count_thresh);
-    console.log(connection_count_thresh);
-}
+form.addEventListener("submit", async () => {
+    console.log("clicked");
 
-form.addEventListener("submit", e => handleSubmit(e));
+    // value of the threshold for accepting connection requests
+    let conn_cnt_thresh = connection_count_thresh.value;
+    chrome.storage.sync.set({ conn_cnt_thresh });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['content.js']
+    });
+});
